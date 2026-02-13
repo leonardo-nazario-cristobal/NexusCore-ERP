b336a53926b72c4280c5c6aa6da75b39c452da80
Leonardo Nazario <leonardonazario533@gmail.com>
2026-02-12 23:26:51 -0600

    Tare: Correccion de " por ''

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $path = __DIR__ . $uri;
 
 // Si exite el archivo fisico → servirlo
-if ($uri !== "/" && file_exists($path)) {
+if ($uri !== '/' && file_exists($path)) {
    return false;
 }
 
